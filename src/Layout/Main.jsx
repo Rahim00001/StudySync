787930ddba0018da6a51f56d1640bc355c318463
Rahim00001 <rahimlpt2022@gmail.com>
2023-11-26 09:53:48 +0600

    footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footers from "../Pages/Shared/Footer/Footers";
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footers></Footers>
         </div>
     );
 };
